cba690cf4860374fab0d900cf602f5828dfce357
elliotedwinmoses2020 <elliotedwinmoses2020@gmail.com>
2024-02-11 15:40:52 +0000

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
   LinkedIn
   Twitter
   Instagram
+  <Footer/>
     </main>
   );
 }
