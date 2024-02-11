377452269aad7cf3ee9ee94fd6018b1f96f716f8
elliotedwinmoses2020 <elliotedwinmoses2020@gmail.com>
2024-02-11 15:38:30 +0000

    initial commit

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return (
+    <div>Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="https://www.frontendmentor.io/profile/MosesElite69" target="_blank">MosesElite69</a>.</div>
+  )
+}
+
+export default Footer        
---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      Jessica Randall
+  London, United Kingdom
+  "Front-end developer and avid reader."
+
+  GitHub
+  Frontend Mentor
+  LinkedIn
+  Twitter
+  Instagram
+    </main>
+  );
+}
