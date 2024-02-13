aa155263f4c8c1007f2946525571b23b58599d6b
elliotedwinmoses2020 <elliotedwinmoses2020@gmail.com>
2024-02-13 15:29:57 +0000

    finished project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,43 @@
 import Image from "next/image";
-import Footer from "./components/Footer";
+import Footer from "./ui/Footer";
 
 export default function Home() {
   return (
-    <main className="">
-      Jessica Randall
-  London, United Kingdom
-  "Front-end developer and avid reader."
-
-  GitHub
-  Frontend Mentor
-  LinkedIn
-  Twitter
-  Instagram
-  <Footer/>
-    </main>
+    <>
+      <main className="main-bg text-white h-[100vh] flex flex-col items-center p-3">
+        <div className="card-bg flex flex-col text-center w-[80%] h-[80%] sm:w-[30%] sm:h-[80%] border-solid rounded-lg p-4 my-auto ">
+          <Image
+            src="/avatar-jessica.jpeg"
+            alt="jessica"
+            width={60}
+            height={50}
+            className="mx-auto rounded-full"
+          />
+          <h1>Jessica Randall</h1>
+          <p className="location text-sm">London, United Kingdom</p>
+          <p className="text-center text-[14px] my-2">
+            "Front-end developer and avid reader."
+          </p>
+          <div className="grid gap-x-6 m-2">
+            <span className="my-2 w-full rounded-md p-2 hover:cursor-pointer hover:bg-blue-400">
+              GitHub
+            </span>
+            <span className="my-2 w-full rounded-md p-2 hover:cursor-pointer hover:bg-blue-400">
+              Frontend Mentor
+            </span>
+            <span className="my-2 w-full rounded-md p-2 hover:cursor-pointer hover:bg-blue-400">
+              LinkedIn
+            </span>
+            <span className="my-2 w-full rounded-md p-2 hover:cursor-pointer hover:bg-blue-400">
+              Twitter
+            </span>
+            <span className="my-2 w-full rounded-md p-2 hover:cursor-pointer hover:bg-blue-400">
+              Instagram
+            </span>
+          </div>
+        </div>
+        <Footer />
+      </main>
+    </>
   );
 }
--- src/app/ui/Footer.tsx
+++ src/app/ui/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div>Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="https://www.frontendmentor.io/profile/MosesElite69" target="_blank">MosesElite69</a>.</div>
+    Coded by <a href="https://www.frontendmentor.io/profile/MosesElite69" className="text-purple-300 hover:text-blue-300" target="_blank">MosesElite69</a>.</div>
   )
 }
 
